5a6cbcf166bc3b14418ae41f1245282fff418589
Soha <40802905+so-ha@users.noreply.github.com>
2020-10-01 15:46:39 +0530

    Update Home.js

--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -1,50 +1,51 @@
 import React, {Component} from 'react';
 import Card from './Card';
+import axios from 'axios';
 // import PropTypes from "prop-types";
 // import { connect } from "react-redux";
 
 class Home extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			nonFriends : []
-		}
-		// const id = this.props.id;
-		// console.log("Constructor id : "+id);
+		this.nonFriends = [];
 	}
+
+	componentDidMount() {
+		let targetUrl = '/api/users/home';
+		axios({
+			method : 'post',
+			url : targetUrl,
+			headers : {
+				"content-type": "application/json"
+			},
+			data : {
+				id : this.props.id
+			}
+		})
+		.then(res => {
+			//got the list of all the users who are not the friends
+			console.log(res);
+			if(res) {
+				console.log("Hi");
+				this.nonFriends = res.data; 
+				// console.log(this.nonFriends);
+			}
+			else {
+				console.log("All are your friends");
+			}
+		})
+		.catch(err => {
+			console.log(err)
+		}
+		);
+	}
+
 	//displays the cards of all those who are not the friends of the user
 
 	render() {
-		const id = this.props.id;
-		console.log("Home id : "+id);
-
+		console.log("Home id : "+this.props.id);
 		//fetch all the non-friends with the given id
-		// var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-  //   	var targetUrl = '/api/users/home';
-		// fetch(targetUrl, {
-		// 	method : 'POST',
-		// 	headers:{'Content-Type' :'application/json'},
-		// 	body : JSON.stringify({
-		// 		id : id
-		// 	})
-		// })
-		// .then(res => res.json()) //check if json is actually needed
-		// .then(data => {
-		// 	//got the list of all the users who are not the friends
-		// 	console.log(data);
-		// 	if(data) {
-		// 		console.log("Hi");
-		// 		this.setState({
-		// 			nonFriends : data
-		// 		})
-		// 	}
-		// 	else {
-		// 		console.log("All are your friends");
-		// 	}
-		// })
-		// .catch(err =>
-		// 	console.log(err)
-		// );
+		
 		return(
 			<div >
 				<Card />
