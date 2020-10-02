cecef139f457f3870aad7699df241a48e19bc278
Soha <40802905+so-ha@users.noreply.github.com>
2020-10-02 21:29:47 +0530

    Update Card.js

--- src/components/dashboard/Card.js
+++ src/components/dashboard/Card.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import UserCard from './UserCard';
 
 class Card extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {
-			name : ''
-		}
 	}
 
 	addFriend = () => {
@@ -36,42 +34,13 @@ class Card extends Component {
 		}
 		);
 	};
-
-	getUserName = () => {
-		let targetUrl = '/api/users/username';
-		axios({
-			method : 'post',
-			url : targetUrl,
-			headers : {
-				"content-type": "application/json"
-			},
-			data : {
-				id : this.props.nonFriendId
-			}
-		})
-		.then(res => {
-			console.log(res);
-			if(res) {
-			}
-			else {
-				console.log("");
-			}
-		})
-		.catch(err => {
-			console.log(err)
-		}
-		);
-	}
 	
 	render() {
 		return(
-			<div class="container" style = {{
+			<div className="container" style = {{
 	    					width : "30%"
 	    				}}>
-				<ul class="collection">
-	    			<li class="collection-item avatar   blue lighten-4">
-	    				<i class="medium material-icons">person</i>
-	    				<p>{this.props.nonFriendId}</p>
+	    			<UserCard nonFriendId={this.props.nonFriendId}/>
 	    			<button
 			          style={{
 			            width: "150px",
@@ -83,11 +52,10 @@ class Card extends Component {
 			          className="btn btn-medium waves-effect waves-light hoverable blue accent-4">
 			          Add Friend
 			        </button>
-	    			</li>
-    			</ul>
 			</div>
 		);
 	};
 }
 
 export default Card;
+
