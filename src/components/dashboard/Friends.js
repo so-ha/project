1b2ac8a24db10dfc5fafe4b3961656728a11dd40
Soha <40802905+so-ha@users.noreply.github.com>
2020-10-03 15:57:05 +0530

    Update Friends.js

--- src/components/dashboard/Friends.js
+++ src/components/dashboard/Friends.js
@@ -1,11 +1,61 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Graph from 'react-graph-vis';
 
 class Friends extends Component {
 	//renders the bubble map of all the friends and friends of the user's friend, basically mapping all the users of the database
 	//fetch all the lists of the friends
 	render() {
-		const id = this.props.id;
-		console.log("friend id : "+id);
+			const graph = {
+		    nodes: [
+		      { id: 1, label: "a1", title: "node 1 tootip text" },
+		      { id: 2, label: "a2", title: "node 2 tootip text" },
+		      { id: 3, label: "a3", title: "node 3 tootip text" },
+		      { id: 4, label: "a4", title: "node 4 tootip text" },
+		      { id: 5, label: "a5", title: "node 5 tootip text" }
+		    ],
+		    edges: [
+		      { from: 1, to: 2 },
+		      { from: 1, to: 3 },
+		      { from: 2, to: 4 },
+		      { from: 2, to: 5 }
+		    ]
+		  };
+		  const options = {
+		    layout: {
+		      hierarchical: true
+		    },
+		    edges: {
+		      color: "#000000"
+		    },
+		    height: "500px"
+		  };
+
+		  const events = {
+		    select: function(event) {
+		      var { nodes, edges } = event;
+		    }
+		  };
+		return(
+			<div>
+				<h1>Hi</h1>
+				<Graph
+			      graph={graph}
+			      options={options}
+			      events={events}
+			      getNetwork={network => {
+			        //  if you want access to vis.js network api you can set the state in a parent component using this property
+			      }}
+			    />
+			</div>
+		);
+	}
+}
+
+export default Friends;
+
+
+// console.log("friend id : "+id);
 
 		//fetch all the non-friends with the given id
 		// var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
@@ -32,51 +82,3 @@ class Friends extends Component {
 		// .catch(err =>
 		// 	console.log(err)
 		// );
-		return(
-			<div>
-				<h1>Hi</h1>
-			</div>
-		);
-	}
-}
-
-export default Friends;
-
-// Use a similar code for the graph visualization using regraph 
-
-// import React from 'react';
-// import { graph, withHtmlPosition } from '@regraph/graph';
-// import { Line } from '@regraph/connections';
-// import { Triangle } from '@regraph/arrowheads';
-// import styles from './styles';
-// const Rect = withHtmlPosition(({ id }) => (
-//   <div id={id} style={styles.rect}>
-//     {id}
-//   </div>
-// ));
-// const Graph = graph({
-//   autoBox: true,
-//   extractBoxesFromNodes: true,
-//   layout: true,
-//   node: { type: Rect },
-// });
-// export default () => (
-//   <Graph
-//     width={186}
-//     height={220}
-//     nodeLayer="html"
-//     nodes={[
-//       { id: 'Source', x: 40, y: 40 },
-//       { id: 'Destination', x: 40, y: 140 },
-//     ]}
-//     connections={[
-//       {
-//         id: 'connection',
-//         type: Line,
-//         strokeWidth: 1,
-//         src: { id: 'Source', padding: 5 },
-//         dst: { id: 'Destination', padding: 5, marker: <Triangle /> },
-//       },
-//     ]}
-//   />
-// );
